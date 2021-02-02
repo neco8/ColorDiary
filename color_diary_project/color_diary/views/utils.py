64027153ed0a83744eca0f44bae3a405051af71d
nekohachi <shogibeshow@gmail.com>
2021-02-02 14:35:21 +0900

    get_previous_urlを修正

--- color_diary_project/color_diary/views/utils.py
+++ color_diary_project/color_diary/views/utils.py
@@ -1,8 +1,13 @@
+from urllib.parse import urlparse
 from django.urls import reverse
 
 
 def get_previous_url(request):
     referer = request.META.get('HTTP_REFERER')
-    if referer and str(referer).startswith(reverse('')): # 前のページが自分のサイトなら返す
-        return referer
+    if referer is None:
+        return reverse('color_diary:diary-index')
+
+    parse = urlparse(referer)
+    if parse.netloc == request.META.get('HTTP_HOST'): # 前のページが自分のサイトなら返す
+        return parse.path
     return reverse('color_diary:diary-index')
