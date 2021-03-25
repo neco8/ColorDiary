5dd1422b2bd2777a759260e7563850e4fe98f7fa
nekohachi <shogibeshow@gmail.com>
2021-03-25 16:54:06 +0900

    デフォルトの色設定追加

--- color_diary_project/color_diary/signals.py
+++ color_diary_project/color_diary/signals.py
@@ -1,9 +1,24 @@
 from django.db.models.signals import post_save
 from .models import Color, User
+from .fields import parse_hex_color
 
 
 def default_color_setting(sender, instance, created, **kwargs):
     default_color = Color.get_default_color()
+    hex_color_list = [
+        'D75674',
+        'F7774D',
+        'F9BB2B',
+        'B7BF19',
+        '00A583',
+        '008FB3',
+        '4D73BB',
+        '9C5DA0',
+    ]
+    for hex_color in hex_color_list:
+        color = Color.objects.create(hex_color=parse_hex_color(hex_color))
+        color.users.add(instance)
+        color.save()
 
     if created:
         default_color.users.add(instance)
