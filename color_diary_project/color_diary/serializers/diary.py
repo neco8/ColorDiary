c88d8afcc961ecf2aa4738dd98d4ede8fe6ca1ec
neco8 <shogibeshow@gmail.com>
2021-04-05 18:12:08 +0900

    diarySerializer内でcolorやuserを展開するよう設定

--- color_diary_project/color_diary/serializers/diary.py
+++ color_diary_project/color_diary/serializers/diary.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
 from ..models import Diary
+from . import ColorSerializer, UserSerializer
 
 
 class DiarySerializer(serializers.ModelSerializer):
+    color = ColorSerializer()
+    user = UserSerializer()
+
     class Meta:
         model = Diary
-        fields = ['id', 'user', 'color', 'color_level', 'created_at', 'updated_at', 'context']
+        fields = ['id', 'color_level', 'created_at', 'updated_at', 'context']
+        read_only_fields = ['user', 'color']
