624ab007647d8abb2579d1b06924528de3427e59
neco8 <shogibeshow@gmail.com>
2021-04-06 18:27:34 +0900

    ColorSerializerでシリアライザのみコードとalphaを取り出しやすくするよう編集

--- color_diary_project/color_diary/serializers/color.py
+++ color_diary_project/color_diary/serializers/color.py
@@ -6,4 +6,16 @@ from ..models import Color
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Color
-        fields = ['id', 'users', 'hex_color']
+        fields = ['id', 'hex_color']
+
+    def to_representation(self, instance):
+        '''
+        split hex_color into code and alpha.
+        #FFF000-1.0 -> {'hex_color_code': 'FFF000', 'alpha': 1.0}
+        '''
+        ret = super().to_representation(instance)
+        ret['hex_color'] = {
+            'code': str(ret['hex_color']).split('-')[0],
+            'alpha': float(str(ret['hex_color']).split('-')[1])
+        }
+        return ret
