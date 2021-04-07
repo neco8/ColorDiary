44d141860868251c7d75ebd4c90e92e78079b167
neco8 <shogibeshow@gmail.com>
2021-04-07 18:43:31 +0900

    rest_frameworkのrouteをapp側に移した

--- color_diary_project/color_diary/urls.py
+++ color_diary_project/color_diary/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from . import views
+
+
+router = routers.DefaultRouter()
+router.register(r'colors', views.ColorViewSet, basename='color')
+router.register(r'diaries', views.DiaryViewSet, basename='diary')
+router.register(r'users', views.UserViewSet)
+
 
 app_name = 'color_diary'
 urlpatterns = [
     path('', views.top, name='top'),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
