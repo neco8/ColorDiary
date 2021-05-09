d696463067ce4c305e99a19ce78e5faa3eb48f95
neco8 <shogibeshow@gmail.com>
2021-05-09 12:16:41 +0900

    ALLOWED_HOSTSの表記方法が間違っていたので修正

--- color_diary_project/color_diary_project/settings/heroku.py
+++ color_diary_project/color_diary_project/settings/heroku.py
@@ -1,7 +1,7 @@
 import dj_database_url
 
 
-ALLOWED_HOSTS.append('.herokuapp.com')
+ALLOWED_HOSTS.append('emotebook.herokuapp.com')
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
